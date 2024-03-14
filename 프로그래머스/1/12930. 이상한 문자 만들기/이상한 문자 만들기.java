eb74c45052a9d7cdea4d287cf9507e80b636643b
HaBoLeum <113341888+HaBoLeum@users.noreply.github.com>
2024-03-14 11:18:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        char[] chr = s.toCharArray();
+        int count =0;
+        for(int i=0; i<chr.length;i++) {
+            if(chr[i]==32) {
+                count=0;
+            } else if(count%2==0) {
+                count++;
+                if(chr[i]>=97&&chr[i]<=122){
+                    chr[i] -=32;
+                }
+                    
+            } else  {
+                count++;
+                if(chr[i]>=65&&chr[i]<=90) {
+                    chr[i] +=32;
+                }
+            }
+            
+        }
+        
+        return answer=String.valueOf(chr);
+    }
+}
