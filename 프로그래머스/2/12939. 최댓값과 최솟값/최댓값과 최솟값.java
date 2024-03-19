dffd5529d383dfca757cb2d5bfa92bfde84d1033
HaBoLeum <113341888+HaBoLeum@users.noreply.github.com>
2024-03-19 13:02:15 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 19.63 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String [] str = s.split(" ");
+        int[] num = new int[str.length];
+        for(int i=0; i<str.length; i++){
+            num[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(num);
+        return answer += num[0] + " " + num[num.length-1];
+    }
+}
