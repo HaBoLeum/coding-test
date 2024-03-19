e182700e92efcaa0a89f61969ea00a1ff2d72709
HaBoLeum <113341888+HaBoLeum@users.noreply.github.com>
2024-03-19 12:22:58 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int num = 0;
+        int remain = 0;
+        boolean temp = true;
+        
+        while(temp) {
+            num=0;
+            if(n%a==0) {
+                num = (n/a)*b;
+                answer+=num;
+                n = num;
+            } else {
+                n += remain;
+                if(n/a==0){
+                    temp = false;
+                }
+                num = (n/a)*b;
+                answer += num;
+                remain = n%a;
+                n = num;
+            } 
+        }
+        
+        return answer;
+    }
+}
