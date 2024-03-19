1b16b6ea349110df48dba371df69fb96246eb94b
HaBoLeum <113341888+HaBoLeum@users.noreply.github.com>
2024-03-19 14:53:21 +0900

    [level 1] Title: 추억 점수, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        int sum = 0;
+        for(int i=0; i<photo.length; i++) {
+            sum=0;
+            for(int j=0; j<photo[i].length; j++) {
+                for(int x=0; x<name.length;x++) {
+                    if(photo[i][j].equals(name[x])) {
+                        sum += yearning[x];
+                    }
+                }
+            }
+            answer[i] = sum;
+        }
+               
+        
+        
+        
+        return answer;
+    }
+}
